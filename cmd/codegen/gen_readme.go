96a5a9bcf52b408b6bfccbb0d13220f6bf2296ab
nitram509 <maki@bitkings.de>
2023-12-26 00:46:21 +0100

    fix path

--- cmd/codegen/gen_readme.go
+++ cmd/codegen/gen_readme.go
@@ -27,24 +27,13 @@ func updateReadme(snc *structNameCollector) {
 		panic(errors.New(fmt.Sprintf("file %s missing: markerStart='%s', markerEnd:'%s'", readmeFileName, markerMarkdownStart, markerMarkdownEnd)))
 	}
 
-	/*
-		| (Go) function |  |
-		|---------------|--|
-		| [lan.GetGenericForwardingEntry](./pkg/tr064/lan/get_address_range.go) |  |
-		| [lan.GetGenericForwardingEntry](./pkg/tr064/lan/get_address_range.go) |  |
-		| [lan.GetGenericForwardingEntry](./pkg/tr064/lan/get_address_range.go) |  |
-		| [lan.GetGenericForwardingEntry](./pkg/tr064/lan/get_address_range.go) |  |
-		| [lan.GetGenericForwardingEntry](./pkg/tr064/lan/get_address_range.go) |  |
-		| [lan.GetGenericForwardingEntry](./pkg/tr064/lan/get_address_range.go) |  |
-		| [lan.GetGenericForwardingEntry](./pkg/tr064/lan/get_address_range.go) |  |
-	*/
 	sb := strings.Builder{}
 	sb.WriteString("| (Go) function |  |\n")
 	sb.WriteString("|---------------|--|\n")
 	for _, md := range snc.soapMetaDataList {
 		goFileName := determineSoapStubFileName(md.deviceType, md.serviceId, md.actionName)
-		sb.WriteString(fmt.Sprintf("| [%s.%s](./pkg/tr064/%s/%s) |  |\n",
-			md.packageName, md.funcName, md.packageName, goFileName))
+		sb.WriteString(fmt.Sprintf("| [%s.%s](./%s) |  |\n",
+			md.packageName, md.funcName, goFileName))
 
 	}
 
