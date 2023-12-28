a7a830997613ae500a53fc7d8ab44c02fc5613c2
nitram509 <maki@bitkings.de>
2023-12-28 16:37:48 +0100

    improve README

--- cmd/gofritz/main.go
+++ cmd/gofritz/main.go
@@ -17,6 +17,6 @@ func main() {
 		panic(err)
 	}
 	for _, host := range hostList {
-		println(fmt.Sprintf("Host: %s, Active:%v", host.XAvmPort, host.Active))
+		println(fmt.Sprintf("Host: %s, Active:%v", host.HostName, host.Active))
 	}
 }
